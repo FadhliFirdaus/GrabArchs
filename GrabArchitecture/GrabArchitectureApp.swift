e8d643d22a89c37042ae44f9ea890633f53c1d1a
fadhli <fadliprestigious@gmail.com>
2024-02-13 23:35:40 +0800

    Initial Commit

---
+++ GrabArchitecture/GrabArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabArchitectureApp.swift
+//  GrabArchitecture
+//
+//  Created by Fadhli Firdaus on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GrabArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
